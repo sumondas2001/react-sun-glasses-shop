2d230b4f23dd4d53aa832c86bc71bdf7c99bf0cc
sumondas2001 <sumonchandradas1010@gmail.com>
2024-08-10 04:53:21 +0600

    not found page go to the home page button complete

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const NotFound = () => {
@@ -6,7 +7,12 @@ const NotFound = () => {
                <div>
                     <h1 className="text-[200px] font-black  text-center mb-10 text-sky-900 ">440</h1>
                     <h1 className="text-8xl font-bold">NOT FOUND</h1>
+                    <div className="text-center">
+                         <Link className="text-xl font-semibold btn-success btn mt-10 " to={"/"}> Home</Link>
+                    </div>
                </div>
+
+
           </div>
      );
 };
